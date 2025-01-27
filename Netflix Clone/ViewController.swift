1e8abbc6b119d4f077334fabace892292ec6d0c1
Rümeysa Tokur <rtokur11@gmail.com>
2025-01-27 17:58:06 +0300

    Initial Commit

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Rumeysa Tokur on 27.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
